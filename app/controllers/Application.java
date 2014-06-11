de83597fa06146c5046c0ee2c5b58c471562408c
ge3k <ge3k@users.noreply.github.com>
2014-06-11 22:28:22 +0530

    Promise done

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,16 @@
 package controllers;
 
 import com.dropbox.core.*;
+import com.typesafe.plugin.RedisPlugin;
 import play.Logger;
+import play.Play;
+import play.libs.F.Promise;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
+import play.mvc.Results;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 import views.html.done;
 import views.html.index;
 
@@ -14,16 +20,15 @@ import java.util.Map;
 
 public class Application extends Controller {
 
-    private static DbxWebAuth webAuth;
-    static Logger.ALogger log = play.Logger.of("Sample");
+    private static Logger.ALogger log = play.Logger.of("Herioux");
 
     public static Result index() {
         return ok(index.render());
     }
 
-    public static Result getFlow() {
-        final String APP_KEY = "";
-        final String APP_SECRET = "";
+    private static DbxWebAuth getDropboxWebAuth() {
+        String APP_KEY = "npv9f72j4ldebvg";
+        String APP_SECRET = "bwc2xenbkuagiee";
         DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
 
         DbxRequestConfig config = new DbxRequestConfig(
@@ -42,18 +47,21 @@ public class Application extends Controller {
                     absoluteURL(Http.Context.current().request(), true);
         }
         DbxSessionStore csrfSessionStore = new DbxPlaySessionStore(session,sessionKey);
-        webAuth = new DbxWebAuth(config,appInfo,redirectUrl,csrfSessionStore);
-        return redirect(webAuth.start());
+        return new DbxWebAuth(config,appInfo,redirectUrl,csrfSessionStore);
     }
 
-    public static Result oAuthCallBack() {
+    public static Result getFlow() {
+        return redirect(getDropboxWebAuth().start(routes.Application.oAuthCallBack().url()));
+    }
+
+    public static Promise<Result> oAuthCallBack() {
         DbxAuthFinish authFinish;
         try {
-            authFinish = webAuth.finish(Http.Context.current().request().queryString());
+            authFinish = getDropboxWebAuth().finish(Http.Context.current().request().queryString());
         }
         catch (DbxWebAuth.BadRequestException ex) {
             log.error("On /dropbox-auth-finish: Bad request: " + ex.getMessage());
-            return status(400, " Bad request.");
+            return Promise.pure(status(400, " Bad request."));
         }
         catch (DbxWebAuth.BadStateException ex) {
             // Send them back to the start of the auth flow.
@@ -67,24 +75,24 @@ public class Application extends Controller {
                 redirectUrl = routes.Application.oAuthCallBack().
                         absoluteURL(Http.Context.current().request(), true);
             }
-            return redirect(redirectUrl);
+            return Promise.pure(redirect(redirectUrl));
         }
         catch (DbxWebAuth.CsrfException ex) {
             log.error("On /dropbox-auth-finish: CSRF mismatch: " + ex.getMessage());
-            return notFound();
+            return Promise.pure(notFound());
         }
         catch (DbxWebAuth.NotApprovedException ex) {
             // When Dropbox asked "Do you want to allow this app to access your
             // Dropbox account?", the user clicked "No".
-            return unauthorized();
+            return Promise.pure(unauthorized());
         }
         catch (DbxWebAuth.ProviderException ex) {
             log.error("On /dropbox-auth-finish: Auth failed: " + ex.getMessage());
-            return status(503, "Error communicating with Dropbox.");
+            return Promise.pure(status(503, "Error communicating with Dropbox."));
         }
         catch (DbxException ex) {
             log.error("On /dropbox-auth-finish: Error getting token: " + ex.getMessage());
-            return status(503, "Error communicating with Dropbox.");
+            return Promise.pure(status(503, "Error communicating with Dropbox."));
         }
         String accessToken = authFinish.accessToken;
         String uid = authFinish.userId;
@@ -92,7 +100,36 @@ public class Application extends Controller {
 
         //todo: store the values in redis
 
-        return ok(done.render()); //todo change this
+        JedisPool pool = Play.application().plugin(RedisPlugin.class).jedisPool();
+        Jedis jedis = pool.getResource();
+
+        jedis.hset("tokens",uid,accessToken);
+
+        Promise<Boolean> performConversionForUser = Promise.promise(() -> processUser(uid,jedis));
+
+        // resultPromise.
+
+        /*
+
+        Promise<Result> exception1 =  resultPromise.recover(new Function<Throwable, Result>() {
+            @Override
+            public Result apply(Throwable throwable) throws Throwable {
+                return null;
+            }
+        });
+*/
+        //Promise<Void> performConversionForUser = //Promise.promise((uid) -> processUser(uid));
+        //now need to use akka to process user data, get the delta
+        //process(uid);
+
+        return performConversionForUser.map(aBoolean -> {
+            if (aBoolean) {
+                return ok(done.render());
+            }
+            else {
+                return Results.TODO;
+            }
+        }); //todo change this
     }
 
     public static Result echoChallenge() {
@@ -107,7 +144,11 @@ public class Application extends Controller {
         return ok(done.render());
     }
 
-    private boolean validateRequest() {
+    private Boolean validateRequest() {
+        return true;
+    }
+
+    private static Boolean processUser(String uid, Jedis jedis) {
         return true;
     }
 }
